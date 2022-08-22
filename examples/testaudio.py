b4b42e8659a62ed3aea28ae8458221624eca1470
charles92027 <charles@lovethatgarlic.com>
2022-08-22 13:58:47 -0700

    updates examples

--- examples/testaudio.py
+++ examples/testaudio.py
@@ -1,11 +1,12 @@
 from pygame import mixer
 
 mixer.init()
-mixer.music.load("../sounds/Honor.mp3")
+mixer.music.load("../sounds/snare.mp3")
 mixer.music.set_volume(1.0)
 
 try :
-	mixer.music.play(loops = -1)	# play forever
+	#mixer.music.play(loops = -1)	# play forever
+	mixer.music.play()
 	while mixer.music.get_busy() == True:
 		pass
 
