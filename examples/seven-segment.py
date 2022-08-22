526fade5d961fec906c40a06c12e082be74f4ce1
charles92027 <charles@lovethatgarlic.com>
2022-08-22 14:47:24 -0700

    updates seven segment example

--- examples/seven-segment.py
+++ examples/seven-segment.py
@@ -1,9 +1,5 @@
-import board, time, threading, atexit
-import RPi.GPIO as GPIO
-
+import board, time
 from adafruit_ht16k33.segments import BigSeg7x4
-from time import strftime
-
 
 lights = [[0b00000001, 0b00000000, 0b00000000, 0b00000000],[0b00000000, 0b00000001, 0b00000000, 0b00000000],[0b00000000, 0b00000000, 0b00000001, 0b00000000],[0b00000000, 0b00000000, 0b00000000, 0b00000001],[0b00000000, 0b00000000, 0b00000000, 0b00000010],[0b00000000, 0b00000000, 0b00000000, 0b00000100],[0b00000000, 0b00000000, 0b00000000, 0b00001000],[0b00000000, 0b00000000, 0b00001000, 0b00000000],[0b00000000, 0b00001000, 0b00000000, 0b00000000],[0b00001000, 0b00000000, 0b00000000, 0b00000000],[0b00010000, 0b00000000, 0b00000000, 0b00000000],[0b00100000, 0b00000000, 0b00000000, 0b00000000]]
 
@@ -11,17 +7,22 @@ i2c = board.I2C()
 display = BigSeg7x4(i2c, address=0x70)
 display.brightness = 1.0
 
+def printHello():
+	time.sleep(.2)
+	display.set_digit_raw(0, 0b01110110)
+	display.set_digit_raw(1, 0b01111001)
+	display.set_digit_raw(2, 0b00110110)
+	display.set_digit_raw(3, 0b00111111)
+	time.sleep(.2)
+	display.fill(0)
 
+printHello()
+printHello()
+printHello()
+printHello()
+printHello()
 
-message = "HELLO   "
-
-display.fill(0)
-display.marquee(message, 0.25, False)
-
-display.fill(0)
-index = 0
-		
-for loops in range(10):
+for loops in range(4):
 	for index in range(11):
 		display.set_digit_raw(0, lights[index][0])
 		display.set_digit_raw(1, lights[index][1])
@@ -30,5 +31,6 @@ for loops in range(10):
 		time.sleep(.1)
 
 display.fill(0)
-display.marquee(message, 0.25, False)
 
+message = "1234567890    "
+display.marquee(message, .25, False)
