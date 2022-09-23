9d44e85e27d8e06548df7e98a1db9450a096a8ab
charles92027 <charles@lovethatgarlic.com>
2022-09-23 13:52:38 -0700

    Adds update and create to website

--- website.py
+++ website.py
@@ -1,5 +1,7 @@
 import os
 from flask import render_template
+from flask import request
+from flask import redirect
 from alarmclock import app
 import sounds
 from sounds import player
@@ -16,6 +18,55 @@ def index():
 	).fetchall()
 
 	return render_template("index.html", alarmList = alarms, soundList = sounds.listSounds())
+
+@app.route("/alarm/<id>", methods=("GET", "POST"))
+def editAlarm(id):
+
+	if request.method == "POST":
+		alarmId = request.form["alarmId"]
+		theTime = request.form["theTime"]
+		startDate = request.form["startDate"]
+		endDate = request.form["endDate"]
+		sound = request.form["sound"]
+		enabled = request.form.get("enabled", 0)
+		db = database.get_db_for_web()
+		db.execute(
+			"UPDATE alarm SET theTime = ?, startDate = ?, endDate = ?, sound = ?, enabled = ? WHERE id = ?;",
+			(theTime, startDate, endDate, sound, enabled, alarmId)
+		)
+		db.commit()
+		return redirect("/")
+
+	db = database.get_db_for_web()
+	alarm = db.execute(
+		"SELECT DISTINCT"
+		" alarm.id, alarm.theTime, alarm.startDate, alarm.endDate, alarm.sound, alarm.enabled"
+		" FROM alarm WHERE alarm.id = {};".format(id)
+	).fetchone()
+
+	return render_template("alarm.html", alarm = alarm, soundList = sounds.listSounds())
+
+@app.route("/alarm/new", methods=("GET", "POST"))
+def newAlarm():
+
+	if request.method == "POST":
+
+		theTime = request.form["theTime"]
+		startDate = request.form["startDate"]
+		endDate = request.form["endDate"]
+		sound = request.form["sound"]
+		enabled = request.form.get("enabled", 0)
+		db = database.get_db_for_web()
+		db.execute(
+			"INSERT INTO alarm (theTime, startDate, endDate, sound, enabled) VALUES (?, ?, ?, ?, ?);",
+			(theTime, startDate, endDate, sound, enabled)
+		)
+		db.commit()
+		return redirect("/")
+
+	alarm = {'id': 0}
+	return render_template("alarm.html", alarm = alarm, soundList = sounds.listSounds())
+
 
 @app.route("/sounds/<sound>/play")
 def playSound(sound):
