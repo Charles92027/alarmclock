73d2af9208924995f5ac2a9744aec5fbe46225d6
charles92027 <charles@lovethatgarlic.com>
2022-10-15 01:59:22 +0800

    save timezones, get timezone list from OS

--- examples/timezone.py
+++ examples/timezone.py
@@ -2,14 +2,29 @@ import os
 import os.path
 import sys
 
+
+
+
+def  listZones():
+
+	import os
+	tz = os.popen("cd /usr/share/zoneinfo/posix && find * -type f -or -type l | sort").read()
+	timeZones = tz.split('\n')
+	print(timeZones)
+
+
 def main(argv):
-  tzname = os.environ.get('TZ')
-  if tzname:
-    print("tzname:", tzname)
-  elif os.path.exists('/etc/timezone'):
-    print("file:", open('/etc/timezone').read())
-  else:
-    sys.exit(1)
+
+	listZones()
+
+	tzname = os.environ.get('TZ')
+	if tzname:
+		print("tzname:", tzname)
+	elif os.path.exists('/etc/timezone'):
+		print("file:", open('/etc/timezone').read())
+	else:
+		sys.exit(1)
 
 if __name__ == '__main__':
-  main(sys.argv)
+	main(sys.argv)
+
